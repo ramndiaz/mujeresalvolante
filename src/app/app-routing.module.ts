e6ded09bdbf837d54f296f1c9550925716bc5579
ramnd <ramndiaz@gmail.com>
2021-02-21 00:12:19 -0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
+import { EbookComponent } from './ebook/ebook.component';
+import { AulaComponent } from './aula/aula.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { InicioComponent} from './inicio/inicio.component';
+import { EquipoComponent} from './equipo/equipo.component';
+import { NosotrosComponent} from './nosotros/nosotros.component';
+import { NoticiasComponent} from './noticias/noticias.component';
+
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { from } from 'rxjs';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path:'', component: InicioComponent},
+  {path:'inicio', component: InicioComponent},
+  {path:'aula', component: AulaComponent},
+  {path:'contacto', component: ContactoComponent},
+  {path: 'ebook', component: EbookComponent},
+  {path: 'equipo', component: EquipoComponent},
+  {path: 'nosotros', component: NosotrosComponent},
+  {path: 'noticias', component: NoticiasComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AulaComponent } from './aula/aula.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { EbookComponent } from './ebook/ebook.component';
+import { EquipoComponent } from './equipo/equipo.component';
+import { FooterComponent } from './footer/footer.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { NoticiasComponent } from './noticias/noticias.component';
+import { SliderComponent } from './slider/slider.component';
+import { TopComponent } from './top/top.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AulaComponent,
+    ContactoComponent,
+    EbookComponent,
+    EquipoComponent,
+    FooterComponent,
+    InicioComponent,
+    NosotrosComponent,
+    NoticiasComponent,
+    SliderComponent,
+    TopComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
