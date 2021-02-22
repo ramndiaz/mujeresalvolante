390c0caad4c72d259800aef793ad8d4c3f451f07
ramnd <ramndiaz@gmail.com>
2021-02-21 21:18:58 -0400

    second commit

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import{NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-slider',
@@ -7,7 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SliderComponent implements OnInit {
 
-  constructor() { }
+  imagenes: any[]=[
+    {name:'nuestra fundadora',
+    img:'assets/imagen1.jpeg',
+    desc:'conoce nuestra fundadora, maria Diaz, quien siempre ha tenido el deseo de ayudar su comunidad'
+    },
+    {name:'nuestras participantes',
+    img:'assets/imagen2.jpeg',
+    desc:'conoce nuestras participantes, por medio de nuestros programas pueden acceder a clases de manejo totalmente gratis'
+    },
+    {name:'nuestros miembros',
+    img:'assets/imagen3.jpeg',
+    desc:'conoce nuestro equipo de heroes que siempre tienen una sonrisa y estan preparados para servirte'
+    },
+    {name:'nuestra comunidad',
+    img:'assets/imagen4.jpeg',
+    desc:'siempre hemos estado para nuestra gente, ese es el lema de nuestra fundadora'
+    }
+  ];
+
+
+  constructor(private_config: NgbCarouselConfig) { }
 
   ngOnInit(): void {
   }
